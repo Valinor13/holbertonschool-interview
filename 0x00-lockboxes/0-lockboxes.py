2b2581022befad8c6a03d06bfbc2d4ebe3a6bb91
Valinor13 <jwcalhoun2@gmail.com>
2021-11-04 20:18:59 -0500

    Update 0-lockboxes.py

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -6,10 +6,21 @@ from typing import List
 
 
 def canUnlockAll(boxes: List[List[int]]) -> bool:
-    """
-    """
-    key_dict = {}
-    sig = 0
+    
+    """ Can Unlock All: Checks to see if all boxes can be unlocked
+            Params:
+                boxes: List[List[int]] - List of lists containing ints
+                                        representing keys that open more boxes
+            Variables:
+                key_dict: dict - stores keys with the boxes they are found in
+                sig: int - signals if a box is accessed before its key 
+                box_num: int - tracks box number to store with keys in dict
+                box: List[int] - each list stored in boxes
+            Return:
+                Boolean indicating success of attempt to open all boxes """
+
+    key_dict: dict = {}
+    sig: int = 0
 
     for box_num, box in enumerate(boxes):
         for key in box:
