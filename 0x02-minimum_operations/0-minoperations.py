4561d10590e0709d4d8a6daeb51dc0d1d2537de0
Valinor13 <jwcalhoun2@gmail.com>
2021-11-23 15:21:54 -0600

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,15 +5,13 @@
 def minOperations(n: int) -> int:
     """ Finds and returns minimum number of operations required """
 
-    if not n or n <= 1:
+    if not n or not isinstance(n, int) or n <= 1:
         return 0
-    loop_count = n
-    op_count = 0
-    while loop_count > 1:
-        if loop_count % 2 == 0:
-            loop_count /= 2
-            op_count += 2
-        else:
-            loop_count -= 1
-            op_count += 1
-    return op_count
+    op_count = 1
+    count_list = []
+    while n > 1:
+        op_count += 1
+        while(n % op_count == 0 and n > 1):
+            n /= op_count
+            count_list.append(op_count)
+    return sum(count_list)
