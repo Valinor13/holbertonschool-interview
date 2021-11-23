36d58d77f9128a8978e6d4cc05e35d7d66f0f2c2
Valinor13 <jwcalhoun2@gmail.com>
2021-11-23 14:38:14 -0600

    Create 0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Minimum Operations Module """
+
+
+def minoperations(n: int) -> int:
+    """ Finds and returns minimum number of operations required """
+
+    if not n or n <= 1:
+        return 0
+    op_count = 0
+    loop_count = 1
+
+    while loop_count < n:
+        if n % loop_count == 0:
+            loop_count += op_count
+            op_count += 1
+        op_count += 1
+    return op_count
