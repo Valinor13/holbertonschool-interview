e913702942ef95a98eb60e91ab558c528daa11ba
Valinor13 <jwcalhoun2@gmail.com>
2021-11-23 15:11:18 -0600

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,8 +8,8 @@ def minOperations(n: int) -> int:
     if not n or n <= 1:
         return 0
     loop_count = n
-    op_count = 2
-    while loop_count != 1:
+    op_count = 0
+    while loop_count > 1:
         if loop_count % 2 == 0:
             loop_count /= 2
             op_count += 2
