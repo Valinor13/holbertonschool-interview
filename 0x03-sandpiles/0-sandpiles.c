a501c6a1657471e76a146b36a6c7492bc6c9e075
Jay Calhoun <jwcalhoun2@gmail.com>
2021-12-02 02:36:51 +0000

    removing helper function

--- 0x03-sandpiles/0-sandpiles.c
+++ 0x03-sandpiles/0-sandpiles.c
@@ -1,46 +1,6 @@
 #include "sandpiles.h"
 
 /**
- * print_grid - Print 3x3 grid
- * @grid: 3x3 grid
- *
- */
-static void print_grid(int grid[3][3])
-{
-	int i, j;
-
-	for (i = 0; i < 3; i++)
-	{
-	for (j = 0; j < 3; j++)
-		{
-			if (j)
-				printf(" ");
-			printf("%d", grid[i][j]);
-		}
-		printf("\n");
-	}
-}
-
-/**
- * combine_grids - takes 2 matrices and sums them into 1
- * @one: matrix 1
- * @two: matrix 2
- * Return: one altered to 3
- */
-int **combine_grids(int one[3][3], int two[3][3])
-{
-	int sumnum;
-
-	for(i = 0; i < 3; i++) {
-		for(j = 0; j < 3; j++) {
-			sumnum = one[i][j] + two[i][j];
-			one[i][j] = sumnum;
-		}
-	}
-	return (one);
-}
-
-/**
  * sandpiles_sum - totals the sum of the sandpiles cell limit is 3
  * @grid1: first sandpile, in a matrix format
  * @grid2: second sandpile, in a matrix format
@@ -48,5 +8,12 @@ int **combine_grids(int one[3][3], int two[3][3])
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-	grid3 = combine_grids(grid1, grid2);
+	int sumnum, i, j;
+
+	for(i = 0; i < 3; i++) {
+		for(j = 0; j < 3; j++) {
+			sumnum = grid1[i][j] + grid2[i][j];
+			grid1[i][j] = sumnum;
+		}
+	}
 }
--- 0x03-sandpiles/sandpiles.h
+++ 0x03-sandpiles/sandpiles.h
@@ -4,4 +4,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
 #endif
