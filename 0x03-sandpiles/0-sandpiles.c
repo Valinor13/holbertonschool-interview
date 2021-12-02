e9bd98fc38eb054873eec260e60d3f0fa02a03ec
Valinor13 <jwcalhoun2@gmail.com>
2021-12-02 12:38:54 -0600

    Update 0-sandpiles.c

--- 0x03-sandpiles/0-sandpiles.c
+++ 0x03-sandpiles/0-sandpiles.c
@@ -3,7 +3,7 @@
 /**
  * print_pile - Print 3x3 grid
  * @grid: 3x3 grid
- *
+ * Return: returns void
  */
 static void print_pile(int grid[3][3])
 {
@@ -26,7 +26,7 @@ static void print_pile(int grid[3][3])
  * combine_piles - Adds 2 sandpiles together
  * @one: the base pile
  * @two: the pile being added
- * Return: returns the base pile
+ * Return: returns void
  */
 void combine_piles(int one[3][3], int two[3][3])
 {
@@ -43,6 +43,33 @@ void combine_piles(int one[3][3], int two[3][3])
 }
 
 /**
+ * grab_topplenum - gets the topplenum for the piles
+ * @one: the sandpile
+ * @sig: the signal pointer for the topple loop
+ * @tpn: the topple number in pointer
+ * Return: returns void
+ */
+void grab_topplenum(int one[3][3], int *sig, int *tpn)
+{
+	int i, j;
+
+	for (i = 0; i < LINECAP; i++)
+		{
+			for (j = 0; j < LINECAP; j++)
+			{
+				if (one[i][j] > NUMCAP)
+				{
+					*tpn = one[i][j];
+					*sig = 2;
+					break;
+				}
+			}
+			if (*sig == 2)
+				break;
+		}
+}
+
+/**
  * sandpiles_sum - totals the sum of the sandpiles cell limit is 3
  * @grid1: first sandpile, in a matrix format
  * @grid2: second sandpile, in a matrix format
@@ -50,27 +77,16 @@ void combine_piles(int one[3][3], int two[3][3])
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-	int NUMCAP = 3, LINECAP = 3, sig = 1;
-	int topplenum, i, j;
+	int sig = 1;
+	int topplenum = 0, *tpn, *sigp;
 
+	tpn = topplenum;
+	sigp = sig;
 	combine_piles(grid1, grid2);
 	while (sig != 0)
 	{
 		sig = 1;
-		for (i = 0; i < LINECAP; i++)
-		{
-			for (j = 0; j < LINECAP; j++)
-			{
-				if (grid1[i][j] > NUMCAP)
-				{
-					topplenum = grid1[i][j];
-					sig = 2;
-					break;
-				}
-			}
-			if (sig == 2)
-				break;
-		}
+		grab_topplenum(grid1, sigp, tpn);
 		if (sig == 2)
 		{
 			print_pile(grid1);
