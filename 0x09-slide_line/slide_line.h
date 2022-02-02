dd65808d4fd1577f82ad4cfa76655b8484522096
Jay Calhoun <jwcalhoun2@gmail.com>
2022-02-01 20:46:58 -0600

    added terminator to header file

--- 0x09-slide_line/slide_line.h
+++ 0x09-slide_line/slide_line.h
@@ -7,6 +7,6 @@
 #define SLIDE_LEFT 0
 #define SLIDE_RIGHT 1
 
-int slide_line(int *line, size_t size, int direction)
+int slide_line(int *line, size_t size, int direction);
 
-#endif SLIDE_LINE
+#endif
