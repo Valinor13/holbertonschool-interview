0bf51996f46ad6391faa0a5511b45bf3a944a77a
Valinor13 <jwcalhoun2@gmail.com>
2022-02-03 14:29:25 -0600

    Update 0-slide_line.c

--- 0x09-slide_line/0-slide_line.c
+++ 0x09-slide_line/0-slide_line.c
@@ -124,7 +124,6 @@ void combine_right(int *line, size_t size)
 
 	for (i = size - 1; i > 0; i--)
 	{
-		mid = tmp = right = 0;
 		printf("i:%lu\n", i);
 		printf("line[i]:%d\n", line[i]);
 		if (line[i])
