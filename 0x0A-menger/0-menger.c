cf71fe302eabbaf4bdf9e83da9fd5bd51ec9ed37
Valinor13 <jwcalhoun2@gmail.com>
2022-02-17 17:03:23 -0600

    Update 0-menger.c

--- 0x0A-menger/0-menger.c
+++ 0x0A-menger/0-menger.c
@@ -1,28 +1,57 @@
 #include "menger.h"
 
 /**
+ * printCol - prints the current column
+ * @size: the size of the carpet
+ * @row: the row position
+ * @col: the col position
+ * Return: returns void
+ */
+void printCol(size, row, col)
+{
+	int div, printId;
+
+	if (col >= 0)
+	{
+		printId = 1;
+		for (div = 1; div < size; div *= 3)
+		{
+			if (((row / div) % 3 == 1) && ((col / div) % 3 == 1))
+				printId = 0;
+		}
+		if (printId)
+			putchar('#');
+		else
+			putchar(' ');
+		printCol(size, row, col - 1);
+	}
+}
+
+/**
+ * printRow - prints the current row
+ * @size: the size of the carpet
+ * @row: the row position
+ * Return: returns void
+ */
+void printRow(size, row)
+{
+	if (row >= 0)
+	{
+		printCol(size, row, size);
+		printRow(size, row - 1);
+	}
+}
+
+/**
  * menger - separates non operable levels
  * @level: the input level of carpet to print
  * Return: returns void
  */
 void menger(int level)
 {
-	switch (level)
-	{
-	case 0:
-		puts("#");
-		break;
-	case 1:
-		printLevelOne(level);
-		break;
-	case 2:
-		printLevelTwo(level);
-		break;
-	case 3:
-		printLevelThree(level);
-		break;
-	case 4:
-		printLevelFour(level);
-		break;
-	}
+	int size, row;
+
+	size = row = pow(3, level);
+
+	printRow(size, row);
 }
