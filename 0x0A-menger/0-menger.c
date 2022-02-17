d56797c9444ccf3d82700f07ff58b550827def94
Valinor13 <jwcalhoun2@gmail.com>
2022-02-17 15:22:51 -0600

    Update 0-menger.c

--- 0x0A-menger/0-menger.c
+++ 0x0A-menger/0-menger.c
@@ -30,6 +30,96 @@ void printOut()
 	printf("###");
 }
 
+/** printLevelThreeHelperIn - reduce lines of code in print function
+ * Return: returns void
+ */
+void printLevelThreeHelperIn()
+{
+	int j;
+
+	printOut();
+	printSpace();
+	printOut();
+	for (j = 0; j < 3; j++)
+		printSpace();
+	printOut();
+	printSpace();
+	printOut();
+}
+
+/** printLevelThreeHelperOut - reduce lines of code in print function
+ * Return: returns void
+ */
+void printLevelThreeHelperOut()
+{
+	int j;
+
+	for (j = 0; j < 3; j++)
+		printOut();
+	for (j = 0; j < 3; j++)
+		printSpace();
+	for (j = 0; j < 3; j++)
+		printOut();
+}
+
+/**
+ * printLevelThreeOut - prints level 3
+ * @level: print levels
+ * Return: returns void
+ */
+void printLevelThreeOut(int level)
+{
+	int i, j, cubed;
+
+	cubed = pow(3, level - 3);
+	for (i = 0; i < cubed; i++)
+		printLevelThreeHelperOut();
+	putchar('\n');
+	for (i = 0; i < cubed; i++)
+	{
+		for (j = 0; j < 3; j++)
+			printIn();
+		for (j = 0; j < 3; j++)
+			printSpace();
+		for (j = 0; j < 3; j++)
+			printIn();
+	}
+	putchar('\n');
+	for (i = 0; i < cubed; i++)
+		printLevelThreeHelperOut();
+	putchar('\n');
+}
+
+/**
+ * printLevelThreeIn - prints level 3
+ * @level: print levels
+ * Return: returns void
+ */
+void printLevelThreeIn(int level)
+{
+	int i, j, cubed;
+
+	cubed = pow(3, level - 3);
+	for (i = 0; i < cubed; i++)
+		printLevelThreeHelperIn();
+	putchar('\n');
+	for (i = 0; i < cubed; i++)
+	{
+		printIn();
+		printSpace();
+		printIn();
+		for (j = 0; j < 3; j++)
+			printSpace();
+		printIn();
+		printSpace();
+		printIn();
+	}
+	putchar('\n');
+	for (i = 0; i < cubed; i++)
+		printLevelThreeHelperIn();
+	putchar('\n');
+}
+
 /**
  * printLevelOne - Prints the level 1 pattern n times
  * @level: level print times
@@ -93,40 +183,10 @@ void printLevelTwo(int level)
  */
 void printLevelThree(int level)
 {
-	int i, j, cubed;
-
-	cubed = pow(3, level - 3);
 	printLevelTwo(level);
-	for (i = 0; i < cubed; i++)
-	{
-		for (j = 0; j < 3; j++)
-			printOut();
-		for (j = 0; j < 3; j++)
-			printSpace();
-		for (j = 0; j < 3; j++)
-			printOut();
-	}
-	putchar('\n');
-	for (i = 0; i < cubed; i++)
-	{
-		for (j = 0; j < 3; j++)
-			printIn();
-		for (j = 0; j < 3; j++)
-			printSpace();
-		for (j = 0; j < 3; j++)
-			printIn();
-	}
-	putchar('\n');
-	for (i = 0; i < cubed; i++)
-	{
-		for (j = 0; j < 3; j++)
-			printOut();
-		for (j = 0; j < 3; j++)
-			printSpace();
-		for (j = 0; j < 3; j++)
-			printOut();
-	}
-	putchar('\n');
+	printLevelThreeOut(level);
+	printLevelThreeIn(level);
+	printLevelThreeOut(level);
 	printLevelTwo(level);
 }
 /**
