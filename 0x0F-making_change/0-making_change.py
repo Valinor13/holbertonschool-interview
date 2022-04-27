f515b2e10474420d0d3b2c3ff9e6ab557caff87d
Jay Calhoun <68342564+Valinor13@users.noreply.github.com>
2022-04-27 14:03:13 -0500

    Update 0-making_change.py

--- 0x0F-making_change/0-making_change.py
+++ 0x0F-making_change/0-making_change.py
@@ -8,3 +8,22 @@
 # return: the minimum number of coins needed to make change for the given total
 #         or -1 if no solution exists
 def makeChange(coins, total):
+    # sort the coins in descending order
+    coins.sort(reverse=True)
+    # initialize the dp array
+    dp = [0] * (total + 1)
+    # set the dp array to all -1
+    for i in range(total + 1):
+        dp[i] = -1
+    # set the dp array to 0 for all values in the coins list
+    for coin in coins:
+        dp[coin] = 0
+    # set the dp array to 1 for all values in the coins list
+    for coin in coins:
+        for i in range(coin, total + 1):
+            if dp[i - coin] != -1:
+                if dp[i] == -1:
+                    dp[i] = dp[i - coin] + 1
+                else:
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
+    return dp[total]
