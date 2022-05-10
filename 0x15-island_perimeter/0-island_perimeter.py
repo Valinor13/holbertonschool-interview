c5bacab31417e507881bc73de98090d929639d38
Jay Calhoun <68342564+Valinor13@users.noreply.github.com>
2022-05-10 14:20:52 -0500

    Adding island perimeter

---
+++ 0x15-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""A module for evaluting 2d arrays"""
+
+
+def island_perimeter(grid):
+    """
+        A simple function that counts border tiles in a 0, 1 matrix
+        and returns the perimeter of the island
+        land is marked with 1, water is marked with 0
+        there is guaranteed to be at least one island in the matrix
+        the matrix borders are all water
+        there will be no islands with water inside (lakes)
+    """
+    # island perimeter using list comphrehension and no nested for loops
+    perimeter = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] == 1:
+                if row == 0 or grid[row - 1][col] == 0:
+                    perimeter += 1
+                if row == len(grid) - 1 or grid[row + 1][col] == 0:
+                    perimeter += 1
+                if col == 0 or grid[row][col - 1] == 0:
+                    perimeter += 1
+                if col == len(grid[0]) - 1 or grid[row][col + 1] == 0:
+                    perimeter += 1
+    return perimeter
---
+++ 0x15-island_perimeter/README.md
@@ -0,0 +1,11 @@
+# 0x0D Island Perimeter
+
+In this project we will cover a common interview question, determining the island perimeter.
+
+## Task 0: Island Perimeter
+
+Given a grid of 1s and 0s, define the island perimeter with 1s representing the land and 0s representing the water.
+
+### Contributors:
+
+[Jay Calhoun](https://github.com/Valinor13)
