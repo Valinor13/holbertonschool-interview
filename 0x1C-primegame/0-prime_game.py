10ef6dc78da28ece3d3a71cfeb066e8a82a8729d
Valinor13 <jwcalhoun2@gmail.com>
2022-05-16 14:58:43 -0500

    Create 0-prime_game.py

---
+++ 0x1C-primegame/0-prime_game.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+  Given a set of consecutive integers starting from 1 up to and including n,
+    Maria & Ben take turns choosing a prime number from the set and removing that number and its multiples from the set.
+    The player that cannot make a move loses the game.
+"""
+
